763315f0996c485c98fb5ac8643c6081999134e8
GiuseppeNobile <giffo.640@gmail.com>
2021-10-15 14:47:43 +0200

    Configurazione EF

--- Week6Test.WebAPI/Startup.cs
+++ Week6Test.WebAPI/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -9,12 +10,25 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using Week6Test.Core.BusinessLayers;
+using Week6Test.Core.Interfaces;
+using Week6Test.EF;
+using Week6Test.EF.Repositories;
 
 namespace Week6Test.WebAPI
 {
     public class Startup
     {
+        public readonly string ApplicationName =
+            Assembly.GetEntryAssembly().GetName().Name;
+
+        public readonly string ApplicationVersion =
+            $"v{Assembly.GetEntryAssembly().GetName().Version.Major}" +
+            $".{Assembly.GetEntryAssembly().GetName().Version.Minor}" +
+            $".{Assembly.GetEntryAssembly().GetName().Version.Build}";
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -26,6 +40,15 @@ namespace Week6Test.WebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddTransient<IMainBusinessLayer, MainBusinessLayer>();
+            services.AddTransient<IClientRepository, EFClientRepository>();
+            services.AddTransient<IOrderRepository, EFOrderRepository>();
+
+            services.AddDbContext<GestioneOrdiniContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("GestioneOrdini"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
