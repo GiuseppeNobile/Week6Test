ead3bb69d5ca8b26ee8e3d7801ada15fa646d649
GiuseppeNobile <giffo.640@gmail.com>
2021-10-15 09:29:06 +0200

    creazione modelli

---
+++ Week6Test.Core/Models/Client.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Week6Test.Core.Models
+{
+    [DataContract]
+    public class Client
+    {
+        [DataMember]
+        public int ClientID { get; set; }
+
+        [DataMember]
+        public string ClientCode { get; set; }
+
+        [DataMember]
+        public string Nome { get; set; }
+
+        [DataMember]
+        public string Cognome { get; set; }
+    }
+}
---
+++ Week6Test.Core/Models/Order.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Week6Test.Core.Models
+{
+    [DataContract]
+    public class Order
+    {
+        [DataMember]
+        public int OrderID { get; set; }
+
+        [DataMember]
+        public DateTime OrderDate { get; set; }
+
+        [DataMember]
+        public string OrderCode { get; set; }
+
+        [DataMember]
+        public string ProductCode { get; set; }
+
+        [DataMember]
+        public decimal Price { get; set; }
+    }
+}
